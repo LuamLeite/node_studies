e658fffeb0773b2982751f5934f09806598befc0
Luam Leite <luamleite@nasajon.com.br>
2021-01-30 00:42:58 -0300

    Defining a function for every request

--- firstApp/first-app.js
+++ firstApp/first-app.js
@@ -1,3 +1,11 @@
-const fs = require('fs'); //filesystem, one of node core-modules
-fs.writeFileSync('hello.txt', 'Hello from Node.js'); //writing to a file.
-console.log('Hello from Node.js');
+// const fs = require('fs'); //filesystem, one of node core-modules
+// fs.writeFileSync('hello.txt', 'Hello from Node.js'); //writing to a file.
+// console.log('Hello from Node.js');
+
+const http = require ('http'); //importing global module
+
+function rqListener(req, res) {
+
+}
+http.createServer(rqListener); //receives a function that will execute for every incoming request
+//We don't need to use () here. it will search for the function with the same name.
