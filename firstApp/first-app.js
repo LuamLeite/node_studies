7830eae6e7538ffb87b327371ebbd3ae8e39e09d
Luam Leite <luamleite@nasajon.com.br>
2021-01-31 01:39:31 -0300

    Setting up a simple server

--- firstApp/first-app.js
+++ firstApp/first-app.js
@@ -1,11 +1,9 @@
-// const fs = require('fs'); //filesystem, one of node core-modules
-// fs.writeFileSync('hello.txt', 'Hello from Node.js'); //writing to a file.
-// console.log('Hello from Node.js');
-
 const http = require ('http'); //importing global module
 
-function rqListener(req, res) {
+//Using an anonymous function so we don't need to declare it separatedely
+const server = http.createServer((req, res) => {
+    console.log('Server started!')
+    console.log(req); //the server info!
+});
 
-}
-http.createServer(rqListener); //receives a function that will execute for every incoming request
-//We don't need to use () here. it will search for the function with the same name.
+server.listen(3001); //it will keep running to listen to requests at the port 3000
